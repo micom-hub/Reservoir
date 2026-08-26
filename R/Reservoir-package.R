2857ee26c1d757029663f5006d98fd4029498f7b
Reiden Magdaleno <119817892+reidenmags@users.noreply.github.com>
2026-08-26 19:40:36 -0400

    Uploading debugged and updated R functions!

--- R/Reservoir-package.R
+++
@@ -1,7 +0,0 @@
-#' @keywords internal
-"_PACKAGE"
-
-#' @useDynLib Reservoir, .registration = TRUE
-#' @importFrom Rcpp sourceCpp
-#' @importFrom stats convolve dgamma rbinom rgamma rlnorm rnbinom rnorm rpois runif quantile sd
-NULL
